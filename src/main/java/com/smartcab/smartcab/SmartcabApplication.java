53cfae4f76c6cf7a8c924a067075e5851a50bfc1
Girish Garg <girish2nd77@gmail.com>
2026-09-23 21:51:34 +0530

    Project Initialization

---
+++ src/main/java/com/smartcab/smartcab/SmartcabApplication.java
@@ -0,0 +1,13 @@
+package com.smartcab.smartcab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartcabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartcabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartcab/smartcab/SmartcabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartcab.smartcab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartcabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
